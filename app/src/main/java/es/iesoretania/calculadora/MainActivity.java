c3da7dfdb9a80ad5ec8a8107bfb4eae9b22fc9bd
jlozban360 <jlozban360@g.educaand.es>
2024-10-18 10:12:56 +0200

    Initial Release

--- app/src/main/java/es/iesoretania/calculadora/MainActivity.java
+++ app/src/main/java/es/iesoretania/calculadora/MainActivity.java
@@ -1,6 +1,8 @@
 package es.iesoretania.calculadora;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import es.iesoretania.calculadora.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ActivityMainBinding binding;
 
     @Override
@@ -26,5 +28,26 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate( getLayoutInflater( ) );
         setContentView( binding.getRoot( ) );
+
+        binding.buttonOne.setOnClickListener( this );
+        binding.buttonTwo.setOnClickListener( this );
+        binding.buttonThree.setOnClickListener( this );
+        binding.buttonFour.setOnClickListener( this );
+        binding.buttonFive.setOnClickListener( this );
+        binding.buttonSix.setOnClickListener( this );
+        binding.buttonSeven.setOnClickListener( this );
+        binding.buttonEight.setOnClickListener( this );
+        binding.buttonNine.setOnClickListener( this );
+        binding.buttonZero.setOnClickListener( this );
+
+        //binding.buttonPlus.setOnClickListener();
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button = (Button) findViewById( view.getId( ) );
+        String text = binding.textView.getText().toString() + button.getText().toString();
+
+        binding.textView.setText(text);
     }
 }

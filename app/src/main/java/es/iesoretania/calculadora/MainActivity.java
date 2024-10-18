073a01d5c87fbb03e9a68d6589ab1ebdfa133b67
jlozban360 <jlozban360@g.educaand.es>
2024-10-18 11:14:16 +0200

    Initial Release

--- app/src/main/java/es/iesoretania/calculadora/MainActivity.java
+++ app/src/main/java/es/iesoretania/calculadora/MainActivity.java
@@ -14,6 +14,7 @@ import es.iesoretania.calculadora.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ActivityMainBinding binding;
+    private Boolean ShouldReset = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +41,101 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding.buttonNine.setOnClickListener( this );
         binding.buttonZero.setOnClickListener( this );
 
-        //binding.buttonPlus.setOnClickListener();
+        binding.buttonPlus.setOnClickListener( this );
+        binding.buttonMinus.setOnClickListener( this );
+        binding.buttonMultiply.setOnClickListener( this );
+        binding.buttonDivide.setOnClickListener( this );
+
+        binding.buttonEquals.setOnClickListener( this );
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick( View view ) {
         Button button = (Button) findViewById( view.getId( ) );
-        String text = binding.textView.getText().toString() + button.getText().toString();
 
-        binding.textView.setText(text);
+        String text = "";
+        if( ShouldReset )
+        {
+            text = button.getText( ).toString();
+            ShouldReset = false;
+        }
+        else
+            text = binding.textView.getText( ).toString( ) + button.getText( ).toString( );
+
+        String[ ] simbolos = { "+", "-", "*", "/" };
+        String simboloGuardado = "";
+
+        if( button.getText( ).toString( ).equals( "=" ) )
+        {
+            text = text.substring( 0, text.length( ) - 1 );
+
+            for( String simbolo : simbolos )
+            {
+                if( text.contains( simbolo ) )
+                {
+                    simboloGuardado = simbolo;
+                    text = text.replace( simbolo, " " );
+                    break;
+                }
+            }
+
+            String[ ] parts = text.split( " " );
+
+            if( parts.length == 2 )
+            {
+                try {
+                    double num1 = Double.parseDouble( parts[ 0 ] );
+                    double num2 = Double.parseDouble( parts[ 1 ] );
+                    double resultado = 0;
+
+                    switch( simboloGuardado )
+                    {
+                        case "+":
+                        {
+                            resultado = num1 + num2;
+                            break;
+                        }
+                        case "-":
+                        {
+                            resultado = num1 - num2;
+                            break;
+                        }
+                        case "*":
+                        {
+                            resultado = num1 * num2;
+                            break;
+                        }
+                        case "/":
+                        {
+                            if( num2 != 0 )
+                                resultado = num1 / num2;
+                            else
+                            {
+                                binding.textView.setText( "Error: División por 0" );
+                                ShouldReset = true;
+                                return;
+                            }
+
+                            break;
+                        }
+                    }
+
+                    //Mostramos el resultado
+                    binding.textView.setText( String.valueOf( resultado ) );
+                    ShouldReset = true;
+                }
+                catch ( NumberFormatException e ) {
+                    ShouldReset = true;
+                    binding.textView.setText( "Formato inválido" );
+                }
+            } else {
+                ShouldReset = true;
+                binding.textView.setText( "Expresión inválida" );
+            }
+        }
+        else {
+            // Si no es el botón "=", solo actualizamos el texto en el TextView
+            binding.textView.setText( text );
+        }
     }
 }
